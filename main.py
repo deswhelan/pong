58262749a6f12054891fa4b0ed25a3a4c2891313
Des Whelan <wheland6@tcd.ie>
2025-10-23 09:49:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO: create Screen class
+# TODO: create Paddle class
+    # move paddle
+    # create second paddle
+# TODO: create Ball class
+    # make it move
+    # detect collision with wall and bounce accordingly
+    # detect collision with paddle
+    # detect when paddle misses (i.e. collision with edge of screen
+# TODO: create Scoreboard class
