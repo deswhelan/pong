e76820d10c4a0f1d4e65ed4628af805de4114ea7
Des Whelan <wheland6@tcd.ie>
2025-10-23 10:34:02 +0100

    create pong court

---
+++ court.py
@@ -0,0 +1,8 @@
+from turtle import Screen
+
+screen = Screen()
+
+# TODO: create Court class
+class Court(screen):
+    def __init__(self):
+        super().__init__()
--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-# TODO: create Screen class
+# from court import Court
+# court = Court()
+
+from turtle import Screen, Turtle
+
+# TODO: move to a new "Court" class
+COURT_LENGTH = 1000
+COURT_WIDTH = 600
+court = Screen()
+court.setup(COURT_LENGTH, COURT_WIDTH)
+court.bgcolor("royal blue")
+
+def erect_net():
+    """Paints a net in the centre of the court"""
+    paint_roller = Turtle()
+    paint_roller.hideturtle()
+    paint_roller.color("white")
+    paint_roller.pensize(5)
+    paint_roller.speed(100)
+    paint_roller.penup()
+
+    for y in range(int(-COURT_WIDTH/2) + 15, int(COURT_WIDTH/2), 40):
+        paint_roller.goto(0, y)
+        paint_roller.pendown()
+        paint_roller.goto(0, y + 20)
+        paint_roller.penup()
+
+erect_net()
+
+
+court.exitonclick()
 # TODO: create Paddle class
     # move paddle
     # create second paddle
