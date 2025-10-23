aa82ff63e40870607d256bb2d79206b22ebcf377
Des Whelan <wheland6@tcd.ie>
2025-10-23 10:44:52 +0100

    move court logic into new Court class

--- court.py
+++ court.py
@@ -1,8 +1,33 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
-screen = Screen()
+COURT_COLOUR = "royal blue"
+COURT_LENGTH = 1000
+COURT_WIDTH = 600
 
 # TODO: create Court class
-class Court(screen):
+class Court:
     def __init__(self):
         super().__init__()
+        self.create_court()
+
+    def create_court(self):
+        court = Screen()
+        court.setup(COURT_LENGTH, COURT_WIDTH)
+        court.bgcolor(COURT_COLOUR)
+        self.paint_net()
+        court.exitonclick()
+
+    def paint_net(self):
+        """Paints a net in the centre of the court"""
+        paint_roller = Turtle()
+        paint_roller.hideturtle()
+        paint_roller.color("white")
+        paint_roller.pensize(5)
+        paint_roller.speed(100)
+        paint_roller.penup()
+
+        for y in range(int(-COURT_WIDTH / 2) + 15, int(COURT_WIDTH / 2), 40):
+            paint_roller.goto(0, y)
+            paint_roller.pendown()
+            paint_roller.goto(0, y + 20)
+            paint_roller.penup()
--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-# from court import Court
-# court = Court()
+from court import Court
 
-from turtle import Screen, Turtle
+court = Court()
 
-# TODO: move to a new "Court" class
-COURT_LENGTH = 1000
-COURT_WIDTH = 600
-court = Screen()
-court.setup(COURT_LENGTH, COURT_WIDTH)
-court.bgcolor("royal blue")
-
-def erect_net():
-    """Paints a net in the centre of the court"""
-    paint_roller = Turtle()
-    paint_roller.hideturtle()
-    paint_roller.color("white")
-    paint_roller.pensize(5)
-    paint_roller.speed(100)
-    paint_roller.penup()
-
-    for y in range(int(-COURT_WIDTH/2) + 15, int(COURT_WIDTH/2), 40):
-        paint_roller.goto(0, y)
-        paint_roller.pendown()
-        paint_roller.goto(0, y + 20)
-        paint_roller.penup()
-
-erect_net()
-
-
-court.exitonclick()
 # TODO: create Paddle class
     # move paddle
     # create second paddle
